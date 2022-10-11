3c894799de17ce7b7dbaf888c7cba00943f2f521
crystal-lpz <106110698+crystal-lpz@users.noreply.github.com>
2022-10-11 12:08:36 -0700

    Added Axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,17 @@
+import axios from "axios";
 import React, { useState } from "react";
 export default function Dictionary() {
   let [searchword, setSearchWord] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
     alert(`searching for ${searchword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleSearchWord(event) {
     setSearchWord(event.target.value);
