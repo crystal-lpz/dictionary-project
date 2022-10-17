91eae81e1129a9a541ebd5d31bfb1177ddd15da6
crystal-lpz <106110698+crystal-lpz@users.noreply.github.com>
2022-10-17 16:18:07 -0700

    Updated CSS

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   return (
     <div className="phonetic">
       <h3>{props.phonetic.text}</h3>
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         audio
       </a>
     </div>
